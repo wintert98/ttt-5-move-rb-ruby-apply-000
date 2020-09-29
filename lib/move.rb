6b792f2e30928c32d61e9aba44b8d83c1b713304
Thomas Winter <wintert98@gmail.com>
2020-09-29 14:05:22 +0000

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,7 @@
+
+
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -7,3 +11,14 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+
+def input_to_index(input)
+  input.to_i - 1
+ 
+end
+
+def move(board, index, value = "X")
+   board[index] = value
+end
+
+
